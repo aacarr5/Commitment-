78938135aee95841907adbcd85adf7ab95a1d243
Andrew Carr <aacarr5@gmail.com>
2017-03-16 12:13:27 -0500

    Add search link

--- js/Landing.js
+++ js/Landing.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 const Landing = React.createClass({
 	render () {
@@ -8,6 +9,7 @@ const Landing = React.createClass({
 				<h3> This is an application built for <a href="http://www.thepeopleslobbyusa.org/">The People's Lobby </a> </h3>
 				<h4> This application is developed by Andrew Carr and is built in React using a Sinatra Backend</h4>
 				<p> To proceed, please login </p>
+				<Link to='/search'> Review all committments </Link>
 			</div>
 		)
 	}

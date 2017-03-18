5bbe82961e17c974602ae488513c19f320925b5c
Andrew Carr <aacarr5@gmail.com>
2017-03-18 12:05:28 -0500

    Create two way data bind

--- js/Search.js
+++ js/Search.js
@@ -4,9 +4,23 @@ import ShowCom from './ShowCom.js'
 
 
 const Search = React.createClass({
+	getInitialState () {
+		return  {
+			searchTerm:'initial search term'
+		}
+	},
+
+	handleSeachTermChange (event) {
+		this.setState({searchTerm:event.target.value})
+	},
+
 	render () {
 		return(
 			<div className='search'>
+				<header className="search-header">
+					<h1> {this.state.searchTerm} </h1>
+					<input onChange={this.handleSeachTermChange} className="search-input" value={this.state.searchTerm} type="text" placeholder='search' />					
+				</header>
 				{preLoad.committments.map((com) => {
 					return(
 						<ShowCom key={com.id} {...com} />

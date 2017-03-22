e7b56781de16ab226a42c2ec4679186c39d776e3
Andrew Carr <aacarr5@gmail.com>
2017-03-22 10:27:13 -0500

    Implement search feature

--- js/Search.js
+++ js/Search.js
@@ -6,7 +6,7 @@ import ShowCom from './ShowCom.js'
 const Search = React.createClass({
 	getInitialState () {
 		return  {
-			searchTerm:'initial search term'
+			searchTerm:''
 		}
 	},
 
@@ -14,20 +14,22 @@ const Search = React.createClass({
 		this.setState({searchTerm:event.target.value})
 	},
 
-	searchFilter (term,array) {
-		array.filter( (element) => {
-			return `${element.firstName} ${element.lastName}`.toUpperCase().indexOf(term.toUpperCase() ) >= 0
- 		})
+	filerResults (array) {
+		array.filter( (com) => {
+			return `${com.firstName} ${com.lastName} ${com.email}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0
+		})
 	},
 
 	render () {
 		return(
 			<div className='search'>
 				<header className="search-header">
-					<h1> Search: </h1>
+					<h1> Search:</h1>
 					<input onChange={this.handleSeachTermChange} className="search-input" value={this.state.searchTerm} type="text" placeholder='search' />					
 				</header>
-				{preLoad.committments.map((com) => {
+				{preLoad.committments.filter((com) => {
+					return `${com.firstName} ${com.lastName} ${com.email}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0
+				}).map((com) => {
 					return(
 						<ShowCom key={com.id} {...com} />
 					)
